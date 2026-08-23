2b49cc94810770c2d118f16d5b94a216e6167aa7
Stavros Papantonakis <lardianos@yahoo.gr>
2026-08-23 18:41:59 +0300

    RBAC API Gateway

--- thesis_examples/Fleet_tracking_project/smart-fleet/api-gateway/src/main.py
+++ thesis_examples/Fleet_tracking_project/smart-fleet/api-gateway/src/main.py
@@ -17,12 +17,25 @@ import os
 import logging
 
 import httpx
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
+import jwt
+from jwt import PyJWKClient, PyJWTError
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+KEYCLOAK_ISSUER_URL = os.getenv( "KEYCLOAK_ISSUER_URL", "http://localhost:8081/realms/smart-fleet", )
+
+KEYCLOAK_JWKS_URL = os.getenv( "KEYCLOAK_JWKS_URL", "http://keycloak:8080/realms/smart-fleet/protocol/openid-connect/certs",)
+
+KEYCLOAK_CLIENT_ID = os.getenv( "KEYCLOAK_CLIENT_ID", "smart-fleet-frontend", )
+
+jwks_client = PyJWKClient(KEYCLOAK_JWKS_URL)
+
+VEHICLE_MANAGEMENT_ROLES = [ "admin", "fleet_manager", ]
+
+LIVE_TRACKING_ROLES = [ "admin", "fleet_manager", "driver", ]
 
 app = FastAPI(
     title="API Gateway Service",
@@ -44,6 +57,7 @@ app.add_middleware(
 FLEET_API_URL = os.getenv( "FLEET_API_URL", "http://fleet-api:8000",)
 
 LIVE_TRACKING_SERVICE_URL = os.getenv( "LIVE_TRACKING_SERVICE_URL", "http://live-tracking-service:8002",)
+
 
 
 async def proxy_request(request: Request, target_url: str) -> Response:
@@ -127,12 +141,112 @@ async def proxy_request(request: Request, target_url: str) -> Response:
         downstream_response.status_code,
     )
 
-    return Response(
-        content=downstream_response.content,
-        status_code=downstream_response.status_code,
-        media_type=content_type,
+    return Response( content=downstream_response.content, status_code=downstream_response.status_code, media_type=content_type, )
+
+def validate_access_token(request: Request):
+    """
+    Ελέγχει αν το request έχει έγκυρο Keycloak JWT.
+
+    Το API Gateway είναι το πρώτο security boundary.
+    Τα εσωτερικά services συνεχίζουν να μένουν απλά και δεν γνωρίζουν Keycloak.
+    """
+
+    authorization_header = request.headers.get("authorization")
+
+    if not authorization_header:
+        raise HTTPException( status_code=401, detail="Missing Authorization header", )
+
+    if not authorization_header.startswith("Bearer "):
+        raise HTTPException( status_code=401, detail="Invalid Authorization header", )
+
+    token = authorization_header.replace("Bearer ", "", 1)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+        payload = jwt.decode( token, signing_key.key,
+                              algorithms=["RS256"], issuer=KEYCLOAK_ISSUER_URL, options={ "verify_aud": False, }, )
+
+        # logger.info("========= Decoded JWT payload %s ============\n",payload)
+
+        if payload.get("azp") != KEYCLOAK_CLIENT_ID:
+            raise HTTPException( status_code=401, detail="Invalid token client", )
+
+        logger.info(
+            "Authenticated user=%s roles=%s",
+            payload.get("preferred_username"),
+            payload.get("realm_access", {}).get("roles", []),
+        )
+
+
+        return payload
+
+    except PyJWTError:
+        raise HTTPException( status_code=401, detail="Invalid or expired token", )
+
+def role_checker(token_payload: dict, allowed_roles: list[str]):
+    """
+    Ελέγχει αν ο authenticated χρήστης έχει έναν από τους επιτρεπόμενους ρόλους.
+
+    Τα roles έρχονται από το Keycloak JWT μέσα στο realm_access.roles.
+    Η συνάρτηση είναι ξεχωριστή για να μένει απλή και ευανάγνωστη η authorization λογική.
+    """
+
+    user_roles = token_payload.get("realm_access", {}).get("roles", [])
+
+    has_allowed_role = False
+
+    for role in allowed_roles:
+        if role in user_roles:
+            has_allowed_role = True
+            break
+
+    if not has_allowed_role:
+        raise HTTPException(
+            status_code=403,
+            detail="Access denied",
+        )
+
+    logger.info(
+        "Authorized user=%s roles=%s allowed_roles=%s",
+        token_payload.get("preferred_username"),
+        user_roles,
+        allowed_roles,
     )
 
+# def require_roles(*allowed_roles: str):
+#     """
+#     Δημιουργεί dependency που επιτρέπει πρόσβαση μόνο σε συγκεκριμένους ρόλους.
+#
+#     Τα roles έρχονται από το Keycloak JWT μέσα στο realm_access.roles.
+#     Έτσι το API Gateway μπορεί να εφαρμόζει authorization πριν προωθήσει
+#     το request στα εσωτερικά services.
+#     """
+#
+#     def role_checker(token_payload: dict = Depends(validate_access_token)):
+#         user_roles = token_payload.get("realm_access", {}).get("roles", [])
+#
+#         has_allowed_role = any(
+#             role in user_roles
+#             for role in allowed_roles
+#         )
+#
+#         if not has_allowed_role:
+#             raise HTTPException(
+#                 status_code=403,
+#                 detail="Access denied",
+#             )
+#
+#         logger.info(
+#             "Authorized user=%s roles=%s allowed_roles=%s",
+#             token_payload.get("preferred_username"),
+#             user_roles,
+#             allowed_roles,
+#         )
+#
+#         return token_payload
+#
+#     return role_checker
 
 @app.get("/health")
 async def health_check():
@@ -148,31 +262,9 @@ async def health_check():
     }
 
 
-@app.api_route(
-    "/api/v1/vehicles/{imei}/latest-position",
-    methods=["GET"],
-)
-async def get_vehicle_latest_position(request: Request, imei: str):
-    """
-    Προωθεί request για την τελευταία γνωστή θέση οχήματος
-    προς το Live Tracking Service.
+@app.api_route( "/api/v1/vehicles", methods=["GET", "POST"],)
 
-    Το API Gateway δεν διαβάζει Redis.
-    Η Redis ανήκει αποκλειστικά στο Live Tracking Service.
-    """
-    target_url = (
-        f"{LIVE_TRACKING_SERVICE_URL}"
-        f"/vehicles/{imei}/latest-position"
-    )
-
-    return await proxy_request(request, target_url)
-
-
-@app.api_route(
-    "/api/v1/vehicles",
-    methods=["GET", "POST"],
-)
-async def vehicles_collection(request: Request):
+async def vehicles_collection( request: Request, token_payload: dict = Depends(validate_access_token), ):
     """
     Προωθεί requests συλλογής οχημάτων προς το Fleet API.
 
@@ -183,22 +275,43 @@ async def vehicles_collection(request: Request):
     - Δημιουργεί νέο όχημα μέσω Fleet API.
     - Το Fleet API παραμένει owner της MongoDB και των vehicle events.
     """
+    role_checker( token_payload, VEHICLE_MANAGEMENT_ROLES,)
+
+    logger.info("========= Decoded JWT payload %s ============\n", token_payload)
+
     target_url = f"{FLEET_API_URL}/vehicles"
 
     return await proxy_request(request, target_url)
 
 
-@app.api_route(
-    "/api/v1/vehicles/{vehicle_id}",
-    methods=["GET", "PUT", "PATCH", "DELETE"],
-)
-async def vehicle_item(request: Request, vehicle_id: str):
+@app.api_route( "/api/v1/vehicles/{vehicle_id}", methods=["GET", "PUT", "PATCH", "DELETE"], )
+async def vehicle_item(request: Request, vehicle_id: str, token_payload: dict = Depends(validate_access_token), ):
     """
     Προωθεί requests συγκεκριμένου οχήματος προς το Fleet API.
 
     Το route μένει generic ώστε να μπορεί να υποστηρίξει και μελλοντικά
     PUT/PATCH/DELETE όταν προστεθούν στο Fleet API.
     """
+
+    role_checker( token_payload, VEHICLE_MANAGEMENT_ROLES,)
+
     target_url = f"{FLEET_API_URL}/vehicles/{vehicle_id}"
+
+    return await proxy_request(request, target_url)
+
+
+@app.api_route( "/api/v1/vehicles/{imei}/latest-position", methods=["GET"], )
+async def get_vehicle_latest_position(request: Request, imei: str, token_payload: dict = Depends(validate_access_token), ):
+    """
+    Προωθεί request για την τελευταία γνωστή θέση οχήματος
+    προς το Live Tracking Service.
+
+    Το API Gateway δεν διαβάζει Redis.
+    Η Redis ανήκει αποκλειστικά στο Live Tracking Service.
+    """
+
+    role_checker( token_payload, LIVE_TRACKING_ROLES, )
+
+    target_url = ( f"{LIVE_TRACKING_SERVICE_URL}"  f"/vehicles/{imei}/latest-position" )
 
     return await proxy_request(request, target_url)
